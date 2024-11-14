124f9ee4e1bc71863b930475eebbff82dc450b6d
yingxie <cheyannexie@gmail.com>
2024-11-14 13:55:30 +0000

    Refactoring run_game() inside AI class with helper methods.

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,20 +29,27 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Fill the screen with background color during each pass through the loop.
+        self.screen.fill(self.bg_color)
 
-            # Fill the screen with background color during each pass through the loop.
-            self.screen.fill(self.bg_color)
+        # Draw the ship on the screen, so the ship appears on top of the background.
+        self.ship.blitme()   
 
-            # Draw the ship on the screen, so the ship appears on top of the background.
-            self.ship.blitme()   
+        # Make the most recently drawn screen visible.
+        # Continually update the display to show the new positions of game elements. 
+        pygame.display.flip()
 
-            # Make the most recently drawn screen visible.
-            # Continually update the display to show the new positions of game elements. 
-            pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
