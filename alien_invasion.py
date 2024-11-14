85826b96099acbcdefcd7a08ff9f0857e8ede80d
yingxie <cheyannexie@gmail.com>
2024-11-14 14:37:41 +0000

    Refactor _check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -40,20 +40,27 @@ class AlienInvasion:
                     sys.exit()
                 # Detect when the arrow key is pressed
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True 
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
+                    self._check_keydown_events(event)
 
                 # Detect when the arrow key is released
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
+                    self._check_keyup_events(event)
 
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True 
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
 
-    def _update_screen(self):
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self): 
         """Update images on the screen, and flip to the new screen."""
         # Fill the screen with background color during each pass through the loop.
         self.screen.fill(self.bg_color)
